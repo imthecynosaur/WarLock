796134e082b05f86a507535519b0d328e5ed2da9
Sina Ghasemi <itsjustsina@gmail.com>
2022-01-24 01:39:35 +0330

    move function fixed

--- Tank.py
+++ Tank.py
@@ -8,7 +8,7 @@ class Tank:
 
     velocity = 8
     image = pygame.image.load(os.path.join('Assets/Images/tank.png'))
-    scale = 0.1
+    scale = 0.07
 
     def __init__(self, x, y):
         self.tank = pygame.transform.scale(
@@ -20,21 +20,39 @@ class Tank:
         win.blit(self.tank, self.rect)
 
     def move(self, destination_x, destination_y):
-        teta = math.atan((destination_y - self.rect.centery) /
-                         (destination_x - self.rect.centerx))
+        teta = math.degrees(math.atan((destination_y - self.rect.centery) /
+                                      (destination_x - self.rect.centerx)))
 
         if (abs((destination_y - self.rect.centery)) < 4 and abs(destination_x - self.rect.centerx) < 4):
             return
         else:
-            if ((destination_x - self.rect.centerx) > 0 or (destination_y - self.rect.centery) > 0):
-                self.rect.centerx += Tank.velocity * math.cos(teta)
-                self.rect.centery += Tank.velocity * math.sin(teta)
+            if((destination_x - self.rect.centerx) > 0 and (destination_y - self.rect.centery) > 0):
+                self.rect.centerx += Tank.velocity * \
+                    math.cos(math.radians(teta))
+                self.rect.centery += Tank.velocity * \
+                    math.sin(math.radians(teta))
+            elif((destination_x - self.rect.centerx) > 0 and (destination_y - self.rect.centery) < 0):
+                self.rect.centerx += Tank.velocity * \
+                    math.cos(math.radians(teta))
+                self.rect.centery += Tank.velocity * \
+                    math.sin(math.radians(teta))
+            elif((destination_x - self.rect.centerx) < 0 and (destination_y - self.rect.centery) < 0):
+                self.rect.centerx -= Tank.velocity * \
+                    math.cos(math.radians(teta))
+                self.rect.centery -= Tank.velocity * \
+                    math.sin(math.radians(teta))
             elif((destination_x - self.rect.centerx) < 0 and (destination_y - self.rect.centery) > 0):
-                self.rect.centerx -= Tank.velocity * math.cos(teta)
-                self.rect.centery += Tank.velocity * math.sin(teta)
-            else:
-                self.rect.centerx -= Tank.velocity * math.cos(teta)
-                self.rect.centery -= Tank.velocity * math.sin(teta)
+                self.rect.centerx -= Tank.velocity * \
+                    math.cos(math.radians(teta))
+                self.rect.centery -= Tank.velocity * \
+                    math.sin(math.radians(teta))
+
+        return teta
 
 
 # tank1 = Tank(200, 300)
+
+# while True:
+#     n = int(input())
+#     m = int(input())
+#     print(tank1.move(n, m))
