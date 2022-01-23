d0336a67286b52645491b85b0600dfa6886e15b1
Sina Ghasemi <itsjustsina@gmail.com>
2022-01-23 20:55:51 +0330

    class Tank Created. move function is partially working.

---
+++ Tank.py
@@ -0,0 +1,40 @@
+from tkinter import Y
+import pygame
+import os
+import math
+
+
+class Tank:
+
+    velocity = 8
+    image = pygame.image.load(os.path.join('Assets/Images/tank.png'))
+    scale = 0.1
+
+    def __init__(self, x, y):
+        self.tank = pygame.transform.scale(
+            Tank.image, (int(Tank.image.get_width() * Tank.scale), int(Tank.image.get_height() * Tank.scale)))
+        self.rect = self.tank.get_rect()
+        self.rect.center = (x, y)
+
+    def draw(self, win):
+        win.blit(self.tank, self.rect)
+
+    def move(self, destination_x, destination_y):
+        teta = math.atan((destination_y - self.rect.centery) /
+                         (destination_x - self.rect.centerx))
+
+        if (abs((destination_y - self.rect.centery)) < 4 and abs(destination_x - self.rect.centerx) < 4):
+            return
+        else:
+            if ((destination_x - self.rect.centerx) > 0 or (destination_y - self.rect.centery) > 0):
+                self.rect.centerx += Tank.velocity * math.cos(teta)
+                self.rect.centery += Tank.velocity * math.sin(teta)
+            elif((destination_x - self.rect.centerx) < 0 and (destination_y - self.rect.centery) > 0):
+                self.rect.centerx -= Tank.velocity * math.cos(teta)
+                self.rect.centery += Tank.velocity * math.sin(teta)
+            else:
+                self.rect.centerx -= Tank.velocity * math.cos(teta)
+                self.rect.centery -= Tank.velocity * math.sin(teta)
+
+
+# tank1 = Tank(200, 300)
