6fbf234a34b8d8434d743952caf52c93da34fac0
Sina Ghasemi <itsjustsina@gmail.com>
2022-01-23 20:56:09 +0330

    Tank and move !

--- game.py
+++ game.py
@@ -1,13 +1,19 @@
 import pygame
 import os
+from Tank import Tank
 
 pygame.display.set_caption("Warlock")
 
 
+tank_1 = Tank(200, 200)
+tank_2 = Tank(500, 345)
+is_moving = False
+
+
 class Game:
     def __init__(self):
-        self.width = 1100
-        self.height = 700
+        self.width = 1422
+        self.height = 800
         self.win = pygame.display.set_mode((self.width, self.height))
         self.tanks = []
         self.projectiles = []
@@ -21,7 +27,20 @@ class Game:
                 if event.type == pygame.QUIT:
                     run = False
 
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 3:
+                        mouse_x, mouse_y = pygame.mouse.get_pos()
+                        is_moving = True
+
             self.draw()
+            tank_1.draw(self.win)
+            try:
+                if is_moving:
+                    tank_1.move(mouse_x, mouse_y)
+                    print(tank_1.rect.centerx, tank_1.rect.centery)
+            except:
+                pass
+            tank_2.draw(self.win)
             pygame.display.update()
 
         pygame.quit()
