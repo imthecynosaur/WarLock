6f0296ee04c9c5b112f2100afca41ce8cd144e9f
Sina Ghasemi <itsjustsina@gmail.com>
2022-01-24 13:55:08 +0330

    tank movement synced with the image (doesn't work)

--- Tank.py
+++ Tank.py
@@ -1,4 +1,4 @@
-from tkinter import Y
+from tkinter import CENTER, Y
 import pygame
 import os
 import math
@@ -11,28 +11,35 @@ class Tank:
     scale = 0.07
 
     def __init__(self, x, y):
-        self.tank = pygame.transform.scale(
+        self.image = pygame.transform.scale(
             Tank.image, (int(Tank.image.get_width() * Tank.scale), int(Tank.image.get_height() * Tank.scale)))
-        self.rect = self.tank.get_rect()
+        self.center_x = x - self.image.get_width() / 2
+        self.center_y = y - self.image.get_height() / 2
+        self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+        self.angle = 0
 
     def draw(self, win):
-        win.blit(self.tank, self.rect)
+        win.blit(self.image, (self.center_x, self.center_y))
 
     def move(self, destination_x, destination_y):
         teta = math.degrees(math.atan((destination_y - self.rect.centery) /
                                       (destination_x - self.rect.centerx)))
 
-        if (abs((destination_y - self.rect.centery)) < 4 and abs(destination_x - self.rect.centerx) < 4):
+        if (abs(destination_y - self.center_y) < 10 and abs(destination_x - self.center_x) < 10):
             return
         else:
-            if((destination_x - self.rect.centerx) > 0):
-                self.rect.centerx += Tank.velocity * \
+            if((destination_x - self.center_x) > 0):
+                self.center_x += Tank.velocity * \
                     math.cos(math.radians(teta))
-                self.rect.centery += Tank.velocity * \
+                self.center_y += Tank.velocity * \
                     math.sin(math.radians(teta))
-            elif((destination_x - self.rect.centerx) < 0):
-                self.rect.centerx -= Tank.velocity * \
+            elif((destination_x - self.center_x) < 0):
+                self.center_x -= Tank.velocity * \
                     math.cos(math.radians(teta))
-                self.rect.centery -= Tank.velocity * \
+                self.center_y -= Tank.velocity * \
                     math.sin(math.radians(teta))
+
+    def rotate(self, destination_x, destination_y):
+        teta = math.degrees(math.atan((destination_y - self.rect.centery) /
+                                      (destination_x - self.rect.centerx)))
